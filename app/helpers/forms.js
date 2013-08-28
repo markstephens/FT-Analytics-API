f7112e909babc9275a1f31c01c579bfbffab6620
Mark Stephens <mark.stephens@ft.com>
2013-08-28 17:39:19 +0100

    Checkbox/Radio field for forms

--- app/helpers/forms.js
+++ app/helpers/forms.js
@@ -36,8 +36,8 @@ function formHelpers(req, res, next) {
         return "";
     }
 
-    function field_group_start(field) {
-        return '<div class="form-group' + (has_error(field) ? " has-error" : "") + '">' + field_error(field);
+    function field_group_start(field, checkbox) {
+        return '<div class="' + (checkbox ? 'checkbox' : 'form-group') + (has_error(field) ? " has-error" : "") + '">' + field_error(field);
     }
 
     function field_group_end() {
@@ -85,6 +85,37 @@ function formHelpers(req, res, next) {
         ].join('');
     }
 
+    function checkbox_or_radio_for(model, type, field, value, checked_value, options) {
+        if (typeof options === "undefined") {
+            options = {};
+        }
+
+        options = merge.object({
+            id: model + '_' + field,
+            name: model + '[' + field + ']',
+            'class' : (options.inline ? '' : 'form-control'),
+        }, options);
+
+        return [
+            '<input',
+            ' type="', type, '"',
+            ' id="', options.id, '"',
+            ' name="', options.name, '"',
+            ' value="' + value + '"',
+            (value === checked_value ? ' checked="checked"' : ''),
+            ' class="' + options['class'] + '"',
+            ' />'
+        ].join('');
+    }
+
+    function checkbox_for(model, field, value, checked_value, options) {
+        return checkbox_or_radio_for(model, 'checkbox', field, value, checked_value, options);
+    }
+
+    function radio_for(model, field, value, checked_value, options) {
+        return checkbox_or_radio_for(model, 'radio', field, value, checked_value, options);
+    }
+
     function text_area_for(model, field, value, options) {
         if (typeof options === "undefined") {
             options = {};
@@ -99,7 +130,7 @@ function formHelpers(req, res, next) {
             rows : 3
         }, options);
 
-        return '<textarea id="' + options.id + '" name="' + options.name + '" placeholder="' + options.placeholder + '" rows="' + options.rows + '" class="' + options['class'] + '">' + value + '</textarea>';
+        return '<textarea id="' + options.id + '" name="' + options.name + '" placeholder="' + options.placeholder + '" rows="' + options.rows + '" class="' + options['class'] + '"' + (options.required ? ' required="required"' : '') + '>' + value + '</textarea>';
     }
 
     function select_for(model, field, value, options) {
@@ -111,20 +142,35 @@ function formHelpers(req, res, next) {
             id: model + '_' + field,
             name: model + '[' + field + ']',
             required : false,
+            multiple : false,
             'class' : 'form-control',
             values : []
         }, options);
 
         var html = [];
 
-        html.push('<select id="' + options.id + '" name="' + options.name + '" class="' + options['class'] + '">');
+        html.push('<select id="' + options.id + '" name="' + options.name + '" class="' + options['class'] + '"' + (options.required ? ' required="required"' : '') + '' + (options.multiple ? ' multiple="multiple"' : '') + '>');
 
         if (options.blank) {
             html.push('<option value="">' + options.blank + '</option>');
         }
 
         options.values.forEach(function (val) {
-            html.push('<option value="' + val + '"' + (val === value ? ' selected="selected"' : '') + '>' + val + '</option>');
+            var selected = '';
+
+            if (typeof value !== "undefined") {
+                if (options.multiple) {
+                    if (value.indexOf(val) > -1) {
+                        selected = ' selected="selected"';
+                    }
+                } else {
+                    if (val === value) {
+                        selected = ' selected="selected"';
+                    }
+                }
+            }
+
+            html.push('<option value="' + val + '"' + selected + '>' + val + '</option>');
         });
 
         html.push('</select>');
@@ -134,26 +180,56 @@ function formHelpers(req, res, next) {
 
     function form_field(type, model, field, value, options) {
         var html = [];
-        html.push(field_group_start(field));
-        html.push(label_for(model, field, options));
-        html.push('<div class="col-lg-8">');
 
-        switch (type) {
-            case 'textarea':
-                html.push(text_area_for(model, field, value, options));
-                break;
-            case 'select':
-                html.push(select_for(model, field, value, options));
-                break;
-            default:
-                html.push(text_field_for(model, field, value, options));
+        html.push(field_group_start(field, ((type === 'radio' || type === 'checkbox') && options.inline)));
+
+        if ((type === 'radio' || type === 'checkbox') && options.inline) {
+            html.push('<div class="col-lg-offset-2">');
+
+            switch (type) {
+                case 'radio':
+                    html.push(radio_for(model, field, value, options.checked_value, options));
+                    break;
+                case 'checkbox':
+                    html.push(checkbox_for(model, field, value, options.checked_value, options));
+                    break;
+            }
+
+            html.push(label_for(model, field, merge.object({ 'class' : '' }, options)));
+
+            if (options.help) {
+                html.push('<span class="help-block">' + options.help + '</span>');
+            }
+
+            html.push('</div>');
+        } else {
+            html.push(label_for(model, field, options));
+            html.push('<div class="col-lg-8">');
+
+            switch (type) {
+                case 'textarea':
+                    html.push(text_area_for(model, field, value, options));
+                    break;
+                case 'select':
+                    html.push(select_for(model, field, value, options));
+                    break;
+                case 'radio':
+                    html.push(radio_for(model, field, value, options.checked_value, options));
+                    break;
+                case 'checkbox':
+                    html.push(checkbox_for(model, field, value, options.checked_value, options));
+                    break;
+                default:
+                    html.push(text_field_for(model, field, value, options));
+            }
+
+            if (options.help) {
+                html.push('<span class="help-block">' + options.help + '</span>');
+            }
+
+            html.push('</div>');
         }
 
-        if (options.help) {
-            html.push('<span class="help-block">' + options.help + '</span>');
-        }
-
-        html.push('</div>');
         html.push(field_group_end());
 
         return html.join('');
@@ -180,6 +256,8 @@ function formHelpers(req, res, next) {
         text_field_for : text_field_for,
         text_area_for : text_area_for,
         select_for : select_for,
+        checkbox_for : checkbox_for,
+        radio_for : radio_for,
         form_field : form_field,
         selected : selected
     });
